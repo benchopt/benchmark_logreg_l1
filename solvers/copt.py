17e5c3bc5c92fa58a36a11778a660c29755ab8c4
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2021-03-06 17:37:00 +0100

    fixes

--- solvers/copt.py
+++ solvers/copt.py
@@ -17,20 +17,28 @@ class Solver(BaseSolver):
 
     parameters = {
         'accelerated': [False, True],
+        'line_search': [False, True],
         'solver': ['pgd', 'svrg', 'saga'],
     }
 
     def skip(self, X, y, lmbd):
-        if X.shape[1] > 50000:
-            return True, "problem too large."
+        if (X.shape[1] > 50_000) and self.solver not in ['svrg', 'saga']:
+            return True, (
+                f"problem too large (n_features={X.shape[1]} > 50000) "
+                f"for solver {self.solver}."
+            )
         if X.shape[1] > X.shape[0] and self.solver in ['svrg', 'saga']:
             msg = (
                 f"n_features ({X.shape[1]}) is bigger than "
                 f"n_samples ({X.shape[0]})"
             )
             return True, msg
-        if self.accelerated and self.solver != "pgd":
-            return True, f"accelerated is not available for {self.solver}"
+        if (self.accelerated or self.line_search) and self.solver != "pgd":
+            return (
+                True,
+                f"accelerated or line_search is not available for "
+                f"{self.solver}"
+            )
 
         return False, None
 
@@ -39,24 +47,35 @@ class Solver(BaseSolver):
         self.X, self.y, self.lmbd = X, y, lmbd
 
         # Make sure we cache the numba compilation.
-        self.run(1)
+        if self.solver in ['svrg', 'saga']:
+            self.run(1)
 
     def run(self, n_iter):
         X, y, solver = self.X, self.y, self.solver
 
         n_features = X.shape[1]
+
+        x0 = np.zeros(n_features)
+        if n_iter == 0:
+            self.beta = x0
+            return
+
         f = copt.loss.LogLoss(X, y)
         g = copt.penalty.L1Norm(self.lmbd / X.shape[0])
 
         warnings.filterwarnings('ignore', category=RuntimeWarning)
 
         if solver == 'pgd':
-            step_size = 1.0 / f.lipschitz
+            if self.line_search:
+                step = 'backtracking'
+            else:
+                def step(x):
+                    return 1.0 / f.lipschitz
             result = cp.minimize_proximal_gradient(
                 f.f_grad,
-                np.zeros(n_features),
+                x0,
                 g.prox,
-                step=lambda x: step_size,
+                step=step,
                 tol=0,
                 max_iter=n_iter,
                 jac=True,
