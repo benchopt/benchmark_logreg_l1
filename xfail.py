c2b37f83f639b30c3732720a89aecd469c865521
tomMoral <thomas.moreau.2010@gmail.com>
2021-03-17 12:08:56 +0100

    CLN remove unused xfail condition

--- xfail.py
+++ xfail.py
@@ -8,12 +8,6 @@ def xfail_test_solver_install(solver_class):
     if solver_class.name.lower() == 'cyanure' and sys.platform == 'darwin':
         pytest.xfail('Cyanure is not easy to install on macos.')
 
-    # Skip test_solver_install for julia in OSX as there is a version
-    # conflict with conda packages for R
-    # See issue benchopt/benchopt#64
-    if 'julia' in solver_class.name.lower() and sys.platform == 'darwin':
-        pytest.xfail('Julia causes segfault on OSX for now.')
-
     # Lightning install is broken on python3.9+.
     # See issue #XX
     if (solver_class.name.lower() == 'lightning'
