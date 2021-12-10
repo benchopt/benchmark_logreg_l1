Change blitz to use iteration, fix lambda_max computation in objective (#12)
* fix lambda_max in objective, use iteration for blitz criterion

* comment on celr install from github

* Update solvers/blitz.py

Co-authored-by: Thomas Moreau <thomas.moreau.2010@gmail.com>

Co-authored-by: Thomas Moreau <thomas.moreau.2010@gmail.com>

objective.py
@@ -21,13 +21,12 @@ class Objective(BaseObjective):
         self.lmbd = self.reg * self._get_lambda_max()
 
     def compute(self, beta):
-        y_X_beta = self.y * self.X.dot(beta.flatten())
+        y_X_beta = self.y * (self.X @ beta.flatten())
         l1 = abs(beta).sum()
         return np.log(1 + np.exp(-y_X_beta)).sum() + self.lmbd * l1
 
     def _get_lambda_max(self):
-        return abs(self.X.T.dot(self.y)).max()
+        return abs(self.X.T @ self.y).max() / 2
 
     def to_dict(self):
         return dict(X=self.X, y=self.y, lmbd=self.lmbd)
-        #           fit_intercept=self.fit_intercept)