ddca00f8d860421c7a29f15eb17d66e92e515f9a
Tanguy Lefort <tanguy_l@outlook.fr>
2022-03-17 13:02:54 +0100

    test debug snampl macos

--- solvers/snapml.py
+++ solvers/snapml.py
@@ -1,7 +1,10 @@
 from benchopt import BaseSolver, safe_import_context
+import warnings
 
 with safe_import_context() as import_ctx:
-    from snapml import LogisticRegression
+    # from snapml import LogisticRegression
+    from sklearn.exceptions import ConvergenceWarning
+    from sklearn.linear_model import LogisticRegression
     import numpy as np
 
 
@@ -9,24 +12,30 @@ class Solver(BaseSolver):
     name = "snapml"
 
     install_cmd = "conda"
-    requirements = ["pip:snapml"]
+    requirements = ["scikit-learn"]
+
+    parameters = {
+        "solver": [
+            # 'saga',
+            "liblinear"
+        ],
+    }
+    parameter_template = "{solver}"
 
     def set_objective(self, X, y, lmbd):
         self.X, self.y, self.lmbd = X, y, lmbd
 
+        warnings.filterwarnings("ignore", category=ConvergenceWarning)
+
         self.clf = LogisticRegression(
-            fit_intercept=False,
-            regularizer=self.lmbd,
+            solver=self.solver,
+            C=1 / self.lmbd,
             penalty="l1",
+            fit_intercept=False,
             tol=1e-12,
-            dual=False,
         )
 
     def run(self, n_iter):
-        if n_iter == 0:
-            self.clf.coef_ = np.zeros(self.X.shape[1])
-            return
-
         self.clf.max_iter = n_iter
         self.clf.fit(self.X, self.y)
 
