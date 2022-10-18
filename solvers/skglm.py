632277a4d72d993a4437dd431187e90d74265056
Badr-MOUFAD <badr.moufad@emines.um6p.ma>
2022-10-18 15:00:54 +0200

    mm remarks

--- solvers/skglm.py
+++ solvers/skglm.py
@@ -3,11 +3,10 @@ from benchopt import safe_import_context
 
 with safe_import_context() as import_ctx:
     import warnings
-    import numpy as np
     from skglm.penalties import L1
     from skglm.datafits import Logistic
     from skglm.solvers import ProxNewton
-    from skglm.utils import compiled_clone
+    from skglm.estimators import GeneralizedLinearEstimator
     from sklearn.exceptions import ConvergenceWarning
 
 
@@ -31,18 +30,19 @@ class Solver(BaseSolver):
         n_samples = self.X.shape[0]
 
         warnings.filterwarnings('ignore', category=ConvergenceWarning)
-        self.l1_penalty = compiled_clone(L1(lmbd / n_samples))
-        self.log_datafit = compiled_clone(Logistic())
-        self.prox_solver = ProxNewton(tol=1e-12, fit_intercept=False)
+        self.estimator = GeneralizedLinearEstimator(
+            datafit=Logistic(),
+            penalty=L1(lmbd / n_samples),
+            solver=ProxNewton(tol=1e-12, fit_intercept=False)
+        )
 
         # Cache Numba compilation
-        self.run(1)
+        self.run(5)
 
     def run(self, n_iter):
-        self.prox_solver.max_iter = n_iter
-        coef = self.prox_solver.solve(self.X, self.y, self.log_datafit,
-                                      self.l1_penalty)[0]
-        self.coef = coef.flatten()
+        self.estimator.solver.max_iter = n_iter
+        self.estimator.fit(self.X, self.y)
+        self.coef = self.estimator.coef_.flatten()
 
     @staticmethod
     def get_next(stop_val):
