633768c0fe1488219f3795f9e74d9edd06d353dc
Badr MOUFAD <65614794+Badr-MOUFAD@users.noreply.github.com>
2022-11-14 11:45:08 +0100

    ENH - use ``skglm`` proximal Newton solver (#31)

--- solvers/skglm.py
+++ solvers/skglm.py
@@ -3,8 +3,10 @@ from benchopt import safe_import_context
 
 with safe_import_context() as import_ctx:
     import warnings
-    import numpy as np
-    from skglm.estimators import SparseLogisticRegression
+    from skglm.penalties import L1
+    from skglm.datafits import Logistic
+    from skglm.solvers import ProxNewton
+    from skglm.estimators import GeneralizedLinearEstimator
     from sklearn.exceptions import ConvergenceWarning
 
 
@@ -14,7 +16,7 @@ class Solver(BaseSolver):
 
     install_cmd = 'conda'
     requirements = [
-        'pip:skglm'
+        'pip:skglm>=0.2',
     ]
     references = [
         'Q. Bertrand and Q. Klopfenstein and P.-A. Bannier and G. Gidel'
@@ -28,22 +30,20 @@ class Solver(BaseSolver):
         n_samples = self.X.shape[0]
 
         warnings.filterwarnings('ignore', category=ConvergenceWarning)
-        self.logreg = SparseLogisticRegression(
-            alpha=self.lmbd / n_samples, max_iter=1, max_epochs=50_000,
-            tol=1e-12, fit_intercept=False, warm_start=False, verbose=False)
+        self.estimator = GeneralizedLinearEstimator(
+            datafit=Logistic(),
+            penalty=L1(lmbd / n_samples),
+            solver=ProxNewton(tol=1e-12, fit_intercept=False)
+        )
 
-        # Cache Numba compilation
-        self.run(1)
+        # Perform 5 iteration of solver to cache Numba compilation
+        # and avoid wiggly objective curves
+        self.run(5)
 
     def run(self, n_iter):
-        if n_iter == 0:
-            self.coef = np.zeros(self.X.shape[1])
-        else:
-            self.logreg.max_iter = n_iter
-            self.logreg.fit(self.X, self.y)
-
-            coef = self.logreg.coef_.flatten()
-            self.coef = coef
+        self.estimator.solver.max_iter = n_iter
+        self.estimator.fit(self.X, self.y)
+        self.coef = self.estimator.coef_.flatten()
 
     @staticmethod
     def get_next(stop_val):
