19564227d7fcffc24a999031606d3ae8b2ab401b
Badr MOUFAD <65614794+Badr-MOUFAD@users.noreply.github.com>
2023-06-28 14:45:43 +0200

    MTN set get_next as a regular method (#35)

--- solvers/skglm.py
+++ solvers/skglm.py
@@ -45,8 +45,7 @@ class Solver(BaseSolver):
         self.estimator.fit(self.X, self.y)
         self.coef = self.estimator.coef_.flatten()
 
-    @staticmethod
-    def get_next(stop_val):
+    def get_next(self, stop_val):
         return stop_val + 1
 
     def get_result(self):
