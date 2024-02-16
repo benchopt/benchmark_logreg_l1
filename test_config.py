c984b85126cf91ea3b319ae675e6be669d3602b5
mathurinm <mathurin.massias@gmail.com>
2024-02-16 11:03:06 +0100

    skip snapml

--- test_config.py
+++ test_config.py
@@ -17,3 +17,6 @@ def check_test_solver_install(solver_class):
         cuda_version = get_cuda_version()
         if cuda_version is None:
             pytest.xfail("Cuml needs a working GPU hardware.")
+    if "snapml" in solver_class.name.lower():
+        if sys.platform == "darwin":
+            pytest.xfail("SNAPML segfaults on MacOS.")
